927c528d7c0557fb0130d7577296bdf8fb776b2f
secondary-smiles <shavkinder@gmail.com>
2022-11-03 18:07:14 -0400

    Add basic gravity
    - Conflicts with drag
    - Maybe add slippery physics material?

--- Assets/Scripts/Player Controller/Player/PlayerController.cs
+++ Assets/Scripts/Player Controller/Player/PlayerController.cs
@@ -3,38 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public Rigidbody playerBody;
 
     public float speed = 12f;
-    public float drag = 6f;
+    public float drag = 12f;
+    public float gravity = 9.8f;
 
-    public float movementMultiplier = 10f;
+    public float movementMultiplier = 20f;
+
+    [System.NonSerialized]
+    public Rigidbody playerBody;
 
     [System.NonSerialized]
     public Vector3 playerVelocity;
 
+    [System.NonSerialized]
+    public bool isGrounded;
+
     // Start is called before the first frame update
     void Start() {
+        playerBody = GetComponent<Rigidbody>();
         playerBody.freezeRotation = true;
+        playerBody.useGravity = false;
 
-        StartupAddComponents();
-    }
-
-    // Update is called once per frame
-    void Update() {
-
+        _startupAddComponents();
     }
 
     private void FixedUpdate() {
         MovePlayer();
+        ApplyGravity();
     }
 
     void MovePlayer() {
-        playerBody.AddForce(playerVelocity.normalized * speed * movementMultiplier, ForceMode.Acceleration);
+        playerBody.AddForce(movementMultiplier * speed * playerVelocity.normalized, ForceMode.Acceleration);
     }
 
-    void StartupAddComponents() {
+    void ApplyGravity() {
+        Vector3 gravityVelocity = new Vector3(0, -1.0f, 0) * playerBody.mass * gravity;
+
+        playerBody.AddForce(gravityVelocity);
+    }
+
+    void _startupAddComponents() {
         gameObject.AddComponent<Move>();
-        gameObject.AddComponent<Gravity>();
     }
 }
