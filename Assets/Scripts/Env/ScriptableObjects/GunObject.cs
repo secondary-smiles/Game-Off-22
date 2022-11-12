4dd691ec3e0fc2156f63fb2e0fc129f22a037918
secondary-smiles <shavkinder@gmail.com>
2022-11-12 13:31:12 -0500

    Add salt melee

--- Assets/Scripts/Env/ScriptableObjects/GunObject.cs
+++ Assets/Scripts/Env/ScriptableObjects/GunObject.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Gun", menuName = "ScriptableObjects/Gun")]
 public class GunObject : ScriptableObject {
     public string gunName;
-
+    public AnimatorOverrideController animator;
     public AnimationClip idle;
-    public AnimationClip walk;
-    public AnimationClip sprint;
     public AnimationClip fire;
     public AnimationClip reload;
 }
